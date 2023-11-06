0c57197cda712e03014c0033f555cc65334574f1
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2023-11-06 19:48:08 +0100

    Initial Code Skeleton for the Project

---
+++ 3. Implementation/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+Recipe:
+1. create a class for a Node
+	- Test that this class can link to the SVG element on the HTML
+	- Test that the class provides functionality like changing the Node Value, Color, Position etc..
+
+2. create a class for an EdgeUI
+	- The EdgeUI class is responsible to draw a line between 2 nodes no matter where are there positions on the Canvas
+	- Test that this class can link to the SVG element on the HTML
+	- Test that the class provides functionality like changing the edge stroke size, color, etc...
+
+3. Hardcode a small graph represented as an Adjacency List
+
+---> Might want to abstract away the usage between the SVG Tags and the Node & EdgeUI Classes
+*/
+
