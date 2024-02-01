8f01d5eb3ba2b6d9b0556cefcb6042245adae363
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2024-02-01 14:07:56 +0100

    implemented bfs visualizer

--- 4. Implementation/script.mjs
+++ 4. Implementation/script.mjs
@@ -2,12 +2,15 @@
 
 import {print, sleep} from "./utils/utils.mjs";
 import {GraphUi, EdgeUi, graphSamples} from "./graph/graph-visualizer.mjs"
+import {BFSVisualizer} from "./graph/algorithm-visualizers/bfs.mjs"
 
 // HTML Elements //
 const graphInput = document.getElementById("graph-data");
 const graphTypeSelect = document.getElementById("graph-type");
 
 const g = new GraphUi();
+const bfsVisualizer = new BFSVisualizer(g);
+
 graphInput.addEventListener("keyup", function (event) {
 	const {edgeList, nodes} = generateGraph(event.target.value)
 	if (event.target.value == "" || ((edgeList.length || nodes.length) && (event.key.length == 1 || event.key == "Backspace")))
@@ -54,9 +57,7 @@ function generateEdge(row) {
 	return [];
 }
 
-// Develop a function that is able to read the node string and convert it to the corresponding node_id
-// if this string doesn't exist in the graph for any reason for example due to an update in the Graph, then an exception is thrown
-document.getElementById("drawing-area").addEventListener("click", function (event) {
+document.getElementById("graph_canvas").addEventListener("click", function (event) {
 	let textEl = event.target;
 	if (event.target.tagName == "circle")
 		textEl = event.target.nextElementSibling;
@@ -64,83 +65,6 @@ document.getElementById("drawing-area").addEventListener("click", function (even
 	if (textEl.tagName != "text")
 		return;
 
-	const text = textEl.textContent;
-	const nodeId = g.getCircleId(text)
-
-	// if (nodeId !== null)
-		// visualize_playing(nodeId);
+	const nodeText = textEl.textContent;
+	bfsVisualizer.startVisualizer(nodeText);
 })
-
-/* Initial Code to Visualize BFS (alot of problems in the code)
-	. Code is not Clean & DRY
-	. Choose better colors for the visualization process
-	. Visualizing BFS in an undirected GRAPH results in an unatural visualization (see about that)
-		- the reason for not having an unatural visualization is that you store edges in reverse directions
-		- some how you need to convert it to a dummy directed graph to simulate a natural visualization
-	. You should provide more methods in the Line Class to be able to customize how the line is displayed
-
-async function visualize_playing(id) {
-	// BFS
-	g.drawGraph()
-
-	const q = [];
-	const vis = new Set();
-
-	const start = id
-	q.push(start);
-	vis.add(start);
-
-	const diGraph = g.getGraph().getDirectedAdjList();
-	for (let sz = q.length; q.length !== 0; sz = q.length) {
-		while (sz--) {
-			const nodeId = q[0];
-			q.shift();
-
-			const curNode = g.getCircle(nodeId);
-			curNode.setColor("lightBlue");
-			g.displayGraph();
-			await sleep(1500);
-
-			for (const neighbour of diGraph[nodeId]) {
-				const neighbourNode = g.getCircle(neighbour);
-				
-				const edge = g.getEdge(nodeId, neighbour);
-				edge.getLine().setStrokeCol("cyan");
-				g.displayGraph();
-				await sleep(1000);
-
-				if (!vis.has(neighbour)) {
-					q.push(neighbour);
-					vis.add(neighbour);
-
-					neighbourNode.setColor("yellow");
-					neighbourNode.display();
-					await sleep(1000);
-
-					edge.getLine().setStrokeWidth(4);
-				} else
-					edge.getLine().setStrokeWidth(2);
-
-				edge.getLine().setStrokeCol("black");
-				g.displayGraph();
-				await sleep(1000);
-			}
-			
-			curNode.setColor("lightGreen");
-			g.displayGraph();
-			await sleep(1000);
-		}
-	}
-}
-
-// visualize_playing();
-*/
-
-// async function main() {
-// 	for (const graph of graphSamples) {
-// 		g.readAdjacencyList(graph);
-// 		g.drawGraph();
-// 		await sleep(5000);
-// 	}
-// }
-// main();
