0df2015b1555ecf2ae6c124a1be8d231646a17f9
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2024-02-02 21:27:58 +0100

    making some code cleanup

--- 4. Implementation/script.mjs
+++ 4. Implementation/script.mjs
@@ -1,8 +1,4 @@
 "use strict";
-
-import {print, sleep} from "./utils/utils.mjs";
-import {GraphUi, EdgeUi, graphSamples} from "./graph/graph-visualizer.mjs"
-import {BFSVisualizer, DFSVisualizer} from "./graph/algorithm-visualizers/traversals.mjs"
 import {FrontendHandler} from "./frontend/frontend.mjs"
 
 function main() {
